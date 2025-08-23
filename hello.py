2caa40fc199e00ca04507c1bcf9571f3b3c0dae4
Q-UBAIDULLAH <qaziubaidullah5@gmail.com>
2025-08-24 03:17:23 +0500

    created

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print("hello world")
+# print("hello world")
+
+# # VARIABLE
+# name="Qazi Ubaid"
+# print(name)
+
+# NAME="Umair"
+# print(NAME)
+
+# num=3.14 
+
+# names1=['qazi','ubaid','ahsan']#mutable
+# names2=('osama','kashif','zia')#immutable
+# names1[0]='qazi ubaid'
+# print(names1)
+# names2[0]="osama khan"
+
+# print(names2)
+
+
+num_list=[1,2,3,4,5,1,2,3,4,5]
+num_set={1,2,3,4,5,1,2,3,4,5}#unique
+print(num_list)
+print(num_set)
